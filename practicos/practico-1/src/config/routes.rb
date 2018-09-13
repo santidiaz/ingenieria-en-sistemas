34dcfefd5e0a35a2cdf2708fff33a3cfb9642d65
Agustín Rodríguez <agurz@icloud.com>
2018-09-13 00:08:17 -0300

    Corrección en routes.rb para que se agreguen las rutas onda restful

--- practicos/practico-1/src/config/routes.rb
+++ practicos/practico-1/src/config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
-  get 'users/update'
-  get 'users/edit'
-  get 'users/destroy'
-  get 'users/index'
-  get 'users/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :users
+  resources :users
 end
