593791af5029cdf0098dac15b5a9c6f5a5cd9174
Agustín Rodríguez <me@agurodriguez.net>
2018-10-23 12:28:04 -0300

    prog de redes ej5pr1

---
+++ programacion-de-redes/practicos/practico-1/src/p1e05/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+
+namespace p1e05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumaDos = 0;
+            int sumaTres = 0;
+
+            Thread sumaDosThread = new Thread(() => sumaDos = SumaDos(1, 2));
+            Thread sumaTresThread = new Thread(() => sumaTres = SumaTres(3, 4, 5));
+
+            sumaDosThread.Start();
+            sumaTresThread.Start();
+
+            sumaDosThread.Join();
+            sumaTresThread.Join();
+
+            Console.WriteLine($"Suma total: {sumaDos + sumaTres}");
+        }
+
+        static int SumaDos(int a, int b)
+        {
+            return a + b;
+        }
+
+        static int SumaTres(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+}
