db2f382f043d848d237cf9c0ab3aa91b673138df
Agustín Rodríguez <me@agurodriguez.net>
2018-10-23 11:08:58 -0300

    programación de redes, ejercicio 4, práctico 1

---
+++ programacion-de-redes/practicos/practico-1/src/p1e04/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace p1e04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // * Primer enfoque
+            //      Utilizando tasks
+            
+            Task task = Task.Factory.StartNew(() =>
+            {
+                for (int i = 0; i < 100; i++) Console.Write(0);
+            });
+
+            task.Wait();
+
+            Console.WriteLine();
+            Console.WriteLine("El thread terminó de imprimir los 100 ceros");
+            
+            // * Segundo enfoque
+            //      Utilizando threads (join)
+
+            Thread thread = new Thread(() => 
+            {
+                for (int i = 0; i < 100; i++) Console.Write(0);
+            });
+
+            thread.Start();
+
+            thread.Join();
+
+            Console.WriteLine();
+            Console.WriteLine("El thread terminó de imprimir los 100 ceros");
+        }
+    }
+}
