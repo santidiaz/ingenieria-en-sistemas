cf46372bf5c6980bce7511a76ef8131be65b2bb7
Agustín Rodríguez <me@agurodriguez.net>
2018-10-23 10:58:03 -0300

    progderedes ejercicio 3, práctico 1

---
+++ programacion-de-redes/practicos/practico-1/src/p1e03/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace p1e03
+{
+    class Program
+    {
+        static bool firstThread = true;
+        static readonly object firstThreadLock = new object();
+
+        static void Main(string[] args)
+        {
+            for (int i  = 0; i < new Random().Next(10, 20); i++)
+            {
+                Thread thread = new Thread((n) =>
+                {
+                    lock(firstThreadLock)
+                    {
+                        if (firstThread)
+                        {
+                            Console.WriteLine("Bienvenidos a Programación de Redes 2018");
+                        }
+                        
+                        firstThread = false;
+                    }
+                });
+
+                thread.Start(i);
+            }
+        }
+    }
+}
