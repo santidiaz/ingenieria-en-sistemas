5eabd35be22dcfd234ec09b9a79f2d412467bc8b
Agustín Rodríguez <me@agurodriguez.net>
2018-10-25 18:50:12 -0300

    [prog-de-redes] ej2, pr2

---
+++ programacion-de-redes/practicos/practico-2/src/p2e02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace p2e02
+{
+    class Program
+    {
+        static readonly object[] locker = new object[] { new object(), new object() };
+        
+        static void Main(string[] args)
+        {
+            var thread01 = new Thread(() => 
+            {
+                lock(locker[0])
+                {
+                    Console.WriteLine("Waiting for thread10");
+                    Thread.Sleep(100);
+                    lock(locker[1])
+                    {
+                        Console.WriteLine("This is not gonna happen");
+                    }
+                }
+            });
+            
+            var thread10 = new Thread(() => 
+            {
+                lock(locker[1])
+                {
+                    Console.WriteLine("Waiting for thread01");
+                    Thread.Sleep(100);
+                    lock(locker[0])
+                    {
+                        Console.WriteLine("This is not gonna happen either");
+                    }
+                }
+            });
+
+            thread01.Start();
+            thread10.Start();
+        }
+    }
+}
